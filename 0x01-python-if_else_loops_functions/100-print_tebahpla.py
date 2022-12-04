c5a46caa0de43d04bae4f43cb337324f998ce304
Evans Ngugi <ngugie649@gmail.com>
2022-12-04 12:41:06 +0000

    error fix

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-i = 0
-for c in range(ord('z'), ord('a') - 1, -1):
-    print("{}".format(chr(c - i)), end="")
-    i = 32 if i == 0 else 0
+for char in range(122, 96, -1):
+    print("{:c}".format(char if (char % 2 == 0) else (char - 32)), end='')
