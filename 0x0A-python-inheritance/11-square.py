3d792cd4021e020402a28f618eba70fb1140fd9f
Evans Ngugi <ngugie649@gmail.com>
2023-01-11 22:20:03 +0000

    Changes

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/python3
 """Defines a Rectangle subclass Square."""
 Rectangle = __import__('9-rectangle').Rectangle
--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/python3
 """Defines an inherited class-checking function."""
 
