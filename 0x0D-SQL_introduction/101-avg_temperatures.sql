6618367331e97edac7f0bceae095e3036ae2b9f9
Evanoken <ngugie649@gmail.com>
2023-02-15 01:27:43 +0000

    the above are advanced operations in mysql

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,6 @@
+-- script that displays the average temperature (Fahrenheit) by city ordered by temperature (descending).
+USE hbtn_0c_0;
+SELECT city, AVG(value) AS avg_temp
+FROM temperatures
+GROUP BY city
+ORDER BY avg_temp DESC;
