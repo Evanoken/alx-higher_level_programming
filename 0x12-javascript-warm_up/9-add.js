8ed2fa435794e638358d5e4a435cdeef5cb6ed4c
Evanoken <ngugie649@gmail.com>
2023-03-16 23:25:43 +0000

    function to add

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const process = require('process');
+const x = parseInt(process.argv[2]);
+const y = parseInt(process.argv[2]);
+
+if (isNaN(x) && isNaN(y) {
+  console.log("This is not an integer");
+}
+else{
+console.log(add(x, y)
+}
+function add(a, b){
+return a+b;
+}
