172c61b3bdcb360020688c521a9dc8b671c9b939
Evanoken <ngugie649@gmail.com>
2023-03-16 23:27:51 +0000

    script to add

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,14 +1,5 @@
 #!/usr/bin/node
-const process = require('process');
-const x = parseInt(process.argv[2]);
-const y = parseInt(process.argv[2]);
-
-if (isNaN(x) && isNaN(y) {
-  console.log("This is not an integer");
+function add (a, b) {
+  console.log(parseInt(a) + parseInt(b));
 }
-else{
-console.log(add(x, y)
-}
-function add(a, b){
-return a+b;
-}
+add(process.argv[2], process.argv[3]);
