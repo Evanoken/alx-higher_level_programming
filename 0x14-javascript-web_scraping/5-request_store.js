4a4c288268702940a7fe962267885c4a85c407ba
Evans Ngugi <95754975+Evanoken@users.noreply.github.com>
2023-05-10 15:51:53 -0700

    Web scrapping project

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,4 +1,14 @@
 #!/usr/bin/node
-const fs = require('fs');
+
 const request = require('request');
-request(process.argv[2]).pipe(fs.createWriteStream(process.argv[3]));
+const fs = require('fs');
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    fs.writeFile(filePath, body, 'utf-8');
+  }
+});
--- 0x14-javascript-web_scraping/README.md
+++ 0x14-javascript-web_scraping/README.md
@@ -1,1 +1,1 @@
-Web Scrapping
+web scrapping
